8f6bba01f3002241cf429fc86a488043e2bb7876
João Pedro Portela Gómez Vaz <jportela.gomez@gmail.com>
2013-04-20 02:24:02 -0300

    Update semaphores.c

--- semaphores.c
+++ semaphores.c
@@ -1,20 +1,20 @@
-/************************** | Objetivos | ****************************
-*												                  *
+/************************** | Objetivos | ***************************
+*                                                                   *
 * - Chamadas devem ser feitas no Process Manager.	                  *
 * - Gerenciar até 10 semáforos (SemaphoreID 0,9).	                  *
 * - Semáforos devem ser nicializados com semInit.	                  *
-* - semStatus retorna o valor atual do semáforo passado              *
-*   (1o parâmetro) e o número de processos bloqueados por ele.       *
-* - Tratamento de erro, verificar se SemaphoreID [0,9],              *
-*   senão, systemcall deve retornar -1 e setar errno com EINVAL.     *
-*												                  *
-**********************************************************************
-*												                  *
-* - Todo o gerenciamento de semáforos deve ser implementado          *
-*   através de envio de mensagens entre processos, ou seja, usando   *
-*   as funções mini_send, mini_rec e mini_notify (kernel/proc.c).    *
-*												                  *
-*********************************************************************/
+* - semStatus retorna o valor atual do semáforo passado             *
+*   (1o parâmetro) e o número de processos bloqueados por ele.      *
+* - Tratamento de erro, verificar se SemaphoreID [0,9],             *
+*   senão, systemcall deve retornar -1 e setar errno com EINVAL.    *
+*												                                            *
+*********************************************************************
+*												                                            *
+* - Todo o gerenciamento de semáforos deve ser implementado         *
+*   através de envio de mensagens entre processos, ou seja, usando  *
+*   as funções mini_send, mini_rec e mini_notify (kernel/proc.c).   *
+*												                                            *
+********************************************************************/
 
 int semDown (int SemaphoreID) {
 
